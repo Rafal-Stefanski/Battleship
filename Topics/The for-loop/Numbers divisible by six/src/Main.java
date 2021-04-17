d5bfb82815f5b2141df7fa3aa49160cfd969b144
Rafał Stefański <rafal-stefanski@wp.pl>
2021-04-17 11:47:53 +0200

    daily update (T)

--- Topics/The for-loop/Numbers divisible by six/src/Main.java
+++ Topics/The for-loop/Numbers divisible by six/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < number; i++) {
+            int num = scanner.nextInt();
+            if (num % 6 == 0) {
+                sum = sum + num;
+            }
+        }
+        System.out.println(sum);
     }
 }
--- Topics/The for-loop/Size of parts/src/Main.java
+++ Topics/The for-loop/Size of parts/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int large = 0;
+        int small = 0;
+        int perfect = 0;
+        int[] control = new int[n]; //{0, -1, -1, 1, 0, 0, 0, 0, 0, 1, 0, -1, 1, -1};
+
+        for (int i = 0; i < control.length; i++) {
+            control[i] = scanner.nextInt();
+        }
+
+        for (int j : control) {
+            if (j == 1) {
+                large++;
+            } else if (j == -1) {
+                small++;
+            } else if (j == 0) {
+                perfect++;
+            }
+        }
+        System.out.println(perfect + " " + large + " " + small);
     }
 }
